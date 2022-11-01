ec70fa71e68bbb2fdb373d681fa6e3dc9f7e9424
potap <potapchuk50@gmail.com>
2022-11-01 11:17:32 +0100

    dz1JS

---
+++ lesson1/HomeWork1/main.js
@@ -0,0 +1,50 @@
+                                                                  // 1
+
+const message = 'hello';
+const name = 'owu';
+const dom = 'com';
+const  country = 'ua';
+const i1 = 1;
+const i2 = 10;
+const s1 = -999;
+const i3 = 123;
+const f1 = 3.14;
+const f2 = 2.7;
+const i4 = 16;
+const bool1 = true;
+const bool2 = false;
+
+console.log(message, name, dom, country, i1, i2, i3, i4, s1, f1, f2, bool1, bool2);
+
+
+
+                                                                 // 2
+
+const firstName = 'Potapchuk';
+const middLeName = 'Dmytro';
+const lastName = 'Volodymyrovych';
+const person = `${firstName} ${middLeName} ${lastName}`
+
+console.log(person);
+
+
+
+                                                                // 3
+
+let a = 100;
+let b = '100';
+let c = true;
+
+ console.log(typeof a);
+ console.log(typeof b);
+ console.log(typeof c);
+
+
+                                                               // 4
+let userName = prompt('Введіть своє ім\'я!!');
+let userMiddName = prompt('Введіть своє ім\'я по батькові!!');
+let age = prompt('Вкажіть скільки вам років!',);
+
+alert(userName);
+alert(userMiddName);
+alert(age);
